21aabfc8c65aff49b7f7a8ea216b1ca053825260
chrislemus <icrislemus@gmail.com>
2022-11-03 12:33:02 -0400

    separate login modal into an independednt functional component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,35 @@
 import type { NextPage } from 'next';
-import { NavBar1 } from '@app/common/components/organisms/navbars/navbar-1';
 import { Button } from '@app/common/components/atoms/button';
 import { useForm } from 'react-hook-form';
 import { useState } from 'react';
 
+const LoginModal: React.FC<{ active: boolean; toggleActive: () => void }> = (
+  p
+) => {
+  return (
+    <div className={`modal ${p.active ? 'is-active' : ''}`}>
+      <div className="modal-background"></div>
+      <div className="modal-card">
+        <header className="modal-card-head">
+          <p className="modal-card-title">Modal title</p>
+          <button className="delete" aria-label="close"></button>
+        </header>
+        <section className="modal-card-body">
+          <p>swsw</p>{' '}
+        </section>
+        <footer className="modal-card-foot">
+          <button className="button is-success">Log In</button>
+          <button className="button" onClick={p.toggleActive}>
+            Cancel
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+};
+
 const Home: NextPage = () => {
-  const [logIn, setLogIn] = useState(false);
+  const [logIn, setLogIn] = useState<boolean>(false);
   const toggleLogin = () => setLogIn((a) => !a);
 
   const {
@@ -13,35 +37,17 @@ const Home: NextPage = () => {
     handleSubmit,
     formState: { errors },
   } = useForm();
+
   const onSubmit = (data) => console.log(data);
 
   return (
     <>
-      {/* <NavBar1 /> */}
       <main>
+        <LoginModal active={logIn} toggleActive={toggleLogin} />
         <section
-          className="section has-background-white-bis columns"
+          className="section has-background-white-bis columns is-vcentered is-multiline"
           style={{ paddingBottom: '7rem' }}
         >
-          <div className={`modal ${logIn ? 'is-active' : ''}`}>
-            <div className="modal-background"></div>
-            <div className="modal-card">
-              <header className="modal-card-head">
-                <p className="modal-card-title">Modal title</p>
-                <button className="delete" aria-label="close"></button>
-              </header>
-              <section className="modal-card-body">
-                <p>swsw</p>{' '}
-              </section>
-              <footer className="modal-card-foot">
-                <button className="button is-success">Log In</button>
-                <button className="button" onClick={toggleLogin}>
-                  Cancel
-                </button>
-              </footer>
-            </div>
-          </div>
-
           <div className="column m-5">
             <h1 className="title is-1 mb-0">Streamline your</h1>
             <h1 className="title is-1 has-text-primary">Job Search</h1>
@@ -50,61 +56,26 @@ const Home: NextPage = () => {
               lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
               fugiat aliqua.
             </p>
-            {/* <Button>Get Started</Button> */}
+
+            <Button>Get Started</Button>
+
+            <button
+              className="button is-light"
+              onClick={toggleLogin}
+              style={{ marginLeft: '1rem' }}
+            >
+              LogIn
+            </button>
           </div>
-          <div className="column p-0">
+          <div className="column ">
             <img
               className="image"
               height="100%"
               src="https://images.unsplash.com/photo-1520333789090-1afc82db536a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80"
               alt="woman-on-phone"
+              style={{ borderRadius: '.5rem' }}
             />
           </div>
-        </section>
-        <section className="section has-background-light columns is-centered pb-6">
-          <form
-            className="box columns column is-8 is-multiline is-centered "
-            onSubmit={handleSubmit(onSubmit)}
-            style={{ marginTop: '-5rem' }}
-          >
-            <div className="column is-10 title">
-              <h1>Get Started in Seconds</h1>
-            </div>
-            <div className="column is-10">
-              <input
-                className="input"
-                type="text"
-                placeholder="email"
-                {...register('email', { required: true })}
-              />
-            </div>
-            <div className="column is-10">
-              <input
-                className="input"
-                type="text"
-                placeholder="password"
-                {...register('password', { required: true })}
-              />
-            </div>
-            <div className="column is-10">
-              <span>
-                Anim aute id magna aliqua ad ad non deserunt sunt. <a>linkto</a>
-                Qui irure qui
-              </span>
-            </div>
-            <div className="column is-10">
-              <button type="submit" className="button is-primary">
-                Sign Up
-              </button>
-              <p className="mt-5 is-clickable">
-                Already have an account?{' '}
-                <span onClick={toggleLogin}>Login</span> Instead.
-              </p>
-
-              {/* <div></div> */}
-              {/* <Button>Sign Up</Button> */}
-            </div>
-          </form>
         </section>
       </main>
     </>
