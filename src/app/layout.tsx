c722c32478474a55f41fa3e0351c9fba6412a7b1
chrislemus <icrislemus@gmail.com>
2022-11-22 15:47:08 -0500

    remove bulma and extracts createTheme/queryClient from layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,13 @@
 'use client';
 import * as React from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { PropsWithChildren } from 'react';
 import { Provider } from 'react-redux';
 import { store } from '@common/store/store';
-import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { theme } from '@common/theme';
+import { queryClient } from '@common/query-client';
 // import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
-export const queryClient = new QueryClient();
-export const theme = createTheme();
 
 export default function RootLayout(p: PropsWithChildren<{}>) {
   return (
