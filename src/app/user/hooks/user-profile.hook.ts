bb9bbcd5de55c411bc43f51cf2ba062841e8ff60
chrislemus <icrislemus@gmail.com>
2023-01-31 11:14:17 -0500

    refactor useUserProfile hook and remove user types(duplicate)

--- src/app/user/hooks/user-profile.hook.ts
+++ src/app/user/hooks/user-profile.hook.ts
@@ -1,12 +1,12 @@
 import { jobQuestApi } from '@api/job-quest';
 import { ApiErrorRes } from '@api/job-quest/types';
-import { useQuery } from '@tanstack/react-query';
-import { UserProfile } from '@app/user/types';
+import { useQuery, UseQueryResult } from '@tanstack/react-query';
+import { UserProfile } from '@api/job-quest/user/dto';
 
 export type UserProfileData = UserProfile;
 export type UserProfileError = ApiErrorRes;
 
-export function useUserProfile() {
+export function useUserProfile(): UseQueryResult<UserProfile, ApiErrorRes> {
   return useQuery<UserProfileData, UserProfileError>({
     queryFn: async () => {
       const res = await jobQuestApi.user.profile();
